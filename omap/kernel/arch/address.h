2cc6c27b292e5b3ce51d9ef55fb0477d46bbbb2f
Nepetalactone <tobias.meusburger@students.fhv.at>
2014-03-14 21:41:49 +0100

    Changed base_address from uint8 to long
    Signed-off-by: Nepetalactone <tobias.meusburger@students.fhv.at>

--- omap/kernel/arch/address.h
+++ omap/kernel/arch/address.h
@@ -6,10 +6,10 @@
 #define address(X, Y) __CONCRETE_ADDRESS(X, Y)
 
 //address of a register
-typedef volatile uint32_t* address;
+typedef volatile long* address;
 
 //base address
-typedef volatile uint8_t base_address;
+typedef volatile long base_address;
 
 //offset to base address
 typedef volatile uint16_t offset;
