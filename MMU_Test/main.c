8d8f05448e362984f166166bf9832397a91ea37c
christophwirtensohn <christoph.wirtensohn@students.fhv.at>
2014-05-12 22:43:02 +0200

    reverten...
    Signed-off-by: christophwirtensohn <christoph.wirtensohn@students.fhv.at>

--- MMU_Test/main.c
+++ MMU_Test/main.c
@@ -6,6 +6,8 @@
 
 int main(void) {
 
+	asm("STMFD R13!, {R0-R1}\r\nMRC p15, #0, R0, c1, c0, #0\r\nORR R0, R0, #1\r\nMCR  p15, #0, R0, c1, c0, #0\r\nLDMFD R13!, {R0-R1}\r\nMOC PC, R14");
+
 	volatile int x=0;
 	while(1)
 	{
