29bd7574e8348cf14e2aed03b792e5aa3ba4157e
sme3863 <a.a@a.a>
2014-05-14 10:30:37 +0200

    sd_card defines

--- os/kernel/arch/command.h
+++ os/kernel/arch/command.h
@@ -10,16 +10,15 @@
 
 
 
-void BIT_SET(base_address a, offset o, uint32_t value);
-void BIT_CLEAR(base_address a, offset o, uint32_t value);
-uint32_t BIT_READ(base_address a, offset o, uint8_t start, uint8_t range);
-void BIT_TOGGLE(base_address a, offset o, uint32_t value);
-void REG_SET(base_address a, offset o, uint32_t value);
-void REG_CLEAR(base_address a, offset o);
-char UART_READ(base_address a, offset o, char character);
-void UART_WRITE(base_address a, offset o, char character);
+void BIT_SET(base_address b, offset o, uint32_t position);
+void BIT_CLEAR(base_address b, offset o, uint32_t position);
+uint32_t BIT_READ(base_address b, offset o, uint8_t start, uint8_t range);
+void BIT_TOGGLE(base_address b, offset o, uint32_t value);
+void REG_SET(base_address b, offset o, uint32_t value);
+void REG_CLEAR(base_address b, offset o);
+char UART_READ(base_address b, offset o, char character);
+void UART_WRITE(base_address b, offset o, char character);
 
-
-void WAIT_FOR_WRITE(base_address a, offset o, uint32_t position);
+void WAIT_FOR_WRITE(base_address b, offset o, uint32_t position);
 
 #endif
