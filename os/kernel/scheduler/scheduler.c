2084a35bb4329994cd1130f79ca749a600a6f8d0
sme3863 <stefan.mennel@students.fhv.at>
2014-06-04 17:52:41 +0200

    asdf

--- os/kernel/scheduler/scheduler.c
+++ os/kernel/scheduler/scheduler.c
@@ -11,27 +11,44 @@ void __kill(process_t* process);
 
 
 uint16_t process_count = 0;
-queue_t* process_ready_queue;
-queue_t* process_table;
+//queue_t* process_ready_queue;
+//queue_t* process_table;
 base_address scheduling_timer = GPTIMER4;
 
 process_t* current_process;
 
 
+process_t* procs[3];
+uint32_t counter = 0;
+
 void init_scheduler(){
-	process_ready_queue = createQueue();
-	process_table = createQueue();
+	//process_ready_queue = createQueue();
+	//process_table = createQueue();
 	//TODO adjust scheduling timer TCRR value
-	timer_quick_init(scheduling_timer,0x02000000, run_next_process,trigger_OverflowMatch);
+	timer_quick_init(scheduling_timer,0x00200000, run_next_process,trigger_OverflowMatch);
 
 }
 
 void start_scheduling(){
-	process_t* idle_process = process_create("idle",  &__idle_process);
-	idle_process->state = RUNNING;
+	process_t* process = (process_t*)malloc(sizeof(process_t));
+	process_stack_t* stack = (process_stack_t*)malloc(sizeof(process_stack_t));
+
+
+	process->name = "idle";
+	process->pID = process_count;
+	process->sp = (void*)stack;
+	process->pc = &__idle_process;
+
+	procs[process_count] = process;
+	process_count++;
+
+	//process_table->enqueue(process_table,process);
+	//process_ready_queue->enqueue(process_ready_queue,process);
+
+	process->state = RUNNING;
 
 	timer_start(scheduling_timer);
-	current_process = idle_process;
+	current_process = process;
 	__idle_process();
 }
 
@@ -54,12 +71,12 @@ process_t* process_create(char* process_name, void* entry_point){
 	process->pc = entry_point;
 	process->state = READY;
 
-
+	procs[process_count] = process;
 	process_count++;
 
 	init_process_asm(process->sp,process->pc);
-	process_table->enqueue(process_table,process);
-	process_ready_queue->enqueue(process_ready_queue,process);
+	//process_table->enqueue(process_table,process);
+	//process_ready_queue->enqueue(process_ready_queue,process);
 	return process;
 }
 
@@ -85,16 +102,25 @@ void __kill(process_t* process){
 	//remove process from queue(s)
 }
 
+
+volatile uint64_t times_switched = 0;
+
+
 void run_next_process(void* lr){
 
-	if(process_ready_queue->size == 0){
-		return; //no other ready processes
+	//if(process_ready_queue->size == 0){
+	//	return; //no other ready processes
+	//}
+	times_switched++;
+	if(times_switched % 10 == 0){
+
+		current_process->state = READY;
+
 	}
-
 	current_process->state = READY;
-	process_ready_queue->enqueue(process_ready_queue,current_process);
-
-	process_t* next_process = process_ready_queue->dequeue(process_ready_queue);
+	//process_ready_queue->enqueue(process_ready_queue,current_process);
+	process_t* next_process = procs[counter];
+	//process_t* next_process = process_ready_queue->dequeue(process_ready_queue);
 	process_t* temp = current_process;
 	current_process = next_process;
 	context_switch_asm(temp->sp,lr, next_process->sp);
