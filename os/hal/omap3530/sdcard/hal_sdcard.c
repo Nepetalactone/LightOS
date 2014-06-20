17b30b4baef06b94f1dc86c83351c6d422c61922
christophwirtensohn <christoph.wirtensohn@students.fhv.at>
2014-06-20 15:21:55 +0200

    asdfasf

--- os/hal/omap3530/sdcard/hal_sdcard.c
+++ os/hal/omap3530/sdcard/hal_sdcard.c
@@ -1,11 +1,8 @@
 #include "hal_sdcard_omap3530.h"
-#include "../power/hal_power.h"
 
-void sdcard_soft_reset(base_address mmc_register) {
-	//SOFTWARE RESET
+
+void sdcard_soft_reset(address mmc_register) {
 	BIT_SET(mmc_register, SYSCONFIG, 1);
-	//WAIT FOR RESET
-	WFC_BIT(MMCHS1, SYSSTATUS, 0);
 }
 
 void sdcard_wake_up() {
@@ -13,11 +10,7 @@ void sdcard_wake_up() {
 }
 
 void sdcard_functional_clock() {
-	ENABLE_SDCARD_FUNCTIONAL_CLOCK;
-}
 
-void sdcard_interface_clock(){
-	ENABLE_SDCARD_INTERFACE_CLOCK;
 }
 
 void sdcard_write_command(base_address mmc_register, uint32_t value) {
@@ -35,3 +28,4 @@ void sdcard_init_end(base_address mmc_register) {
 void sdcard_clear_stat_register(base_address mmc_register) {
 	REG_SET(mmc_register, STAT, 0xFFFFFFFF)
 }
+
